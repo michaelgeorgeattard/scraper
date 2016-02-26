2a42d15917dcc08385b001968d49d999749d4319
michaelgeorgeattard <michaelgeorgeattard@gmail.com>
2016-02-26 15:42:27 +0100

    More changes

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
 var config = require('./wikipedia.json');
 var util = require('util');
+var _ = require("lodash");
+var https = require("https");
+var rp = require('request-promise');
+var Promise = require("bluebird");
 
-//var _ = require("lodash");
+var obj = {};
 
-console.log(util.inspect(config, false, null));
+function decode(item, obj) {
+    console.log()    
+ 
+    if (item.url) {
+        retrieve(item.url)
+            .then(response => {
+                obj.data = response;
+            })
+            .then(() => {
+                console.log(obj);
+                
+                _.each(item.data, (subItem) => {
+                    decode(subItem, obj["data"] = {})
+                });
+            });
+    }
+}
 
-_
+function retrieve(url) {
+    return rp(url)
+        .then(function (htmlString) {
+            return htmlString;
+        })
+        .catch(function (err) {
+            // Crawling failed... 
+        });
+}
+
+_.each(config, (item) => {
+    decode(item, obj);
+});
